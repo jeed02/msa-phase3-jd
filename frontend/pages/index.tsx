b993b50f93276e1e97c969b0465b12e687201806
JD <jdbriones22@gmail.com>
2022-09-02 20:37:38 +1200

    Undid Revert

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,49 +5,24 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Character } from "../models/Character";
 import CharCard from "../components/CharCard";
+import SearchBar from "../components/SearchBar";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const [charName, setCharName] = useState<string>("");
-  const [charInput, setCharInput] = useState<string>("");
   const [charInfo, setCharInfo] = useState<Character | null>(null);
-  const GENSHIN_BASE_API_URL = "https://api.genshin.dev/characters/";
 
   return (
     <div className="main">
       <h1 className="title">Genshin Impact Search</h1>
       <div>
-        <TextField
-          id="search-bar"
-          className="text"
-          value={charInput}
-          onChange={(prop: any) => {
-            let name = prop.target.value;
-            setCharInput(name);
-            setCharName(prop.target.value.replace(/\s+/g, "-").toLowerCase());
-          }}
-          label="Enter a Character Name..."
-          variant="outlined"
-          placeholder="Search..."
-          size="small"
-          sx={{
-            "& .MuiInputLabel-root": { color: "white" }, //styles the label
-            "& .MuiOutlinedInput-root": {
-              "& > fieldset": { borderColor: "white" },
-            },
-            "& .MuiOutlinedInput-input": { color: "white" },
-          }}
+        <SearchBar
+          charName={charName}
+          setCharName={setCharName}
+          charInfo={charInfo}
+          setCharInfo={setCharInfo}
         />
-        <IconButton
-          aria-label="search"
-          onClick={() => {
-            search();
-          }}
-        >
-          <SearchIcon style={{ fill: "white" }} />
-        </IconButton>
         {charInfo === null ? (
           <p className="not-found">Character not found</p>
         ) : (
@@ -56,24 +31,6 @@ const Home: NextPage = () => {
       </div>
     </div>
   );
-  function search() {
-    console.log(charName);
-    if (charName === undefined || charName === "") {
-      return;
-    } else {
-      setCharName(charName.replace(/\s+/g, "-").toLowerCase());
-    }
-
-    axios
-      .get(GENSHIN_BASE_API_URL + charName?.toLowerCase())
-      .then((res) => {
-        let info = JSON.parse(JSON.stringify(res.data));
-        setCharInfo(info);
-      })
-      .catch(() => {
-        setCharInfo(null);
-      });
-  }
 };
 
 export default Home;
