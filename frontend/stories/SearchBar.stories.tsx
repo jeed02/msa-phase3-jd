97b869c9b13d26da42f02e2f5362a70a3cb05808
JD <jdbriones22@gmail.com>
2022-09-04 00:29:39 +1200

    Added SearchBar Stories, need to reinstall node modules

--- frontend/stories/SearchBar.stories.tsx
+++ frontend/stories/SearchBar.stories.tsx
@@ -3,6 +3,7 @@ import { Character } from "../models/Character";
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import "../styles/globals.css";
+import { screen, userEvent } from "@storybook/testing-library";
 
 export default {
   title: "SearchBar",
@@ -12,3 +13,14 @@ export default {
 const Template: ComponentStory<typeof SearchBar> = (args) => (
   <SearchBar {...args} />
 );
+
+const Search = Template.bind({});
+Search.play = async () => {
+  const searchBarInput = screen.getByLabelText("Enter a Character Name...");
+
+  await userEvent.type(searchBarInput, "Amber", { delay: 100 });
+
+  const submitButton = screen.getByRole("button");
+
+  await userEvent.click(submitButton);
+};
